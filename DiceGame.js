5d0efca2a950f55ea4e0089218540d5ed125d131
IseTrade <IseTrade@gmail.com>
2018-09-11 13:31:23 -0500

    Premise of the game...

--- DiceGame.js
+++ DiceGame.js
@@ -1,4 +1,13 @@
+// Game premise:
+// The program will randomly pick a number between 50 and 100 and present to player.
+// The player can choose to roll any of the dice, either 6, 8, 10, 12 or 20. 
+// The result of the roll is subtracted from the initial chosen number.
+// If the result ends up to be 0, then the player "wins" the game.
+// If it's less than 0, then he "loses" the game...
+
 let numberOfSides;
+let initialRandomNumber = Math.floor (Math.random() * 100) + 50;
+let gameNumber = initialRandomNumber;
 numberOfSides = prompt ("Please Enter Number of Sides for your die");
 
 function rollDie (numberOfSides) {
@@ -6,4 +15,8 @@ function rollDie (numberOfSides) {
     return roll;
 }
 
-console.log (rollDie(numberOfSides));
+
+let roll = rollDie (numberOfSides);
+
+console.log (roll);
+
