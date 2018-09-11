24d7b98020b1f443c72f8652fda0b66677dccad2
IseTrade <IseTrade@gmail.com>
2018-09-11 10:31:52 -0500

    Initial codes

---
+++ DiceGame.js
@@ -0,0 +1,31 @@
+
+
+function rollDie (numberOfSides) {
+    let roll = Math.floor (Math.random () * numberOfSides) + 1;
+    return roll;
+})
+
+function roll4SidedDie() {
+    let roll = Math.floor (Math.random () *4) + 1;
+    return roll;
+}
+
+function roll6SidedDie () {
+    let roll = Math.floor (Math.random () * 6) + 1;
+    return roll;
+}
+
+function roll8SidedDie () {
+    let roll = Math.floor (Math.random () * 8) + 1;
+    return roll;
+}
+
+function roll10SidedDie () {
+    let roll = Math.floor (Math.random () * 10) + 1;
+    return roll;
+}
+
+function roll12SidedDie () {
+    let roll = Math.floor (Math.random () * 12) + 1;
+    return roll;
+}
