de6249e9019da0c5f3a701468bf824b064f94dc1
IseTrade <IseTrade@gmail.com>
2018-09-11 10:46:47 -0500

    Some changes

--- DiceGame.js
+++ DiceGame.js
@@ -1,31 +1,9 @@
-
+let numberOfSides;
+numberOfSides = prompt ("Please Enter Number of Sides for your die");
 
 function rollDie (numberOfSides) {
     let roll = Math.floor (Math.random () * numberOfSides) + 1;
     return roll;
-})
-
-function roll4SidedDie() {
-    let roll = Math.floor (Math.random () *4) + 1;
-    return roll;
 }
 
-function roll6SidedDie () {
-    let roll = Math.floor (Math.random () * 6) + 1;
-    return roll;
-}
-
-function roll8SidedDie () {
-    let roll = Math.floor (Math.random () * 8) + 1;
-    return roll;
-}
-
-function roll10SidedDie () {
-    let roll = Math.floor (Math.random () * 10) + 1;
-    return roll;
-}
-
-function roll12SidedDie () {
-    let roll = Math.floor (Math.random () * 12) + 1;
-    return roll;
-}
+console.log (rollDie(numberOfSides));
